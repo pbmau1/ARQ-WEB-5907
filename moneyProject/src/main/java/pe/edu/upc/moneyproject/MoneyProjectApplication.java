8de2dce3472958707878fbc6039ce15c41226a67
AngieAMacotela <u202322328@upc.edu.pe>
2025-09-07 20:27:01 -0500

    entorno

---
+++ moneyProject/src/main/java/pe/edu/upc/moneyproject/MoneyProjectApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.moneyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyProjectApplication.class, args);
+    }
+
+}
