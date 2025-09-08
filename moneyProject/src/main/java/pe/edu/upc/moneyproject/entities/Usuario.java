41bba58c991b2aea1b017a43051ed63eab2facb5
mau <mauanto007@gmail.com>
2025-09-07 22:29:31 -0500

    agregue los atributos de Usuario

--- moneyProject/src/main/java/pe/edu/upc/moneyproject/entities/Usuario.java
+++ moneyProject/src/main/java/pe/edu/upc/moneyproject/entities/Usuario.java
@@ -1,4 +1,37 @@
 package pe.edu.upc.moneyproject.entities;
 
 public class Usuario {
+    private String nombre;
+    private String correo;
+    private String contrasenia;
+
+    public Usuario(String nombre, String correo, String contrasenia) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contrasenia = contrasenia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
 }
