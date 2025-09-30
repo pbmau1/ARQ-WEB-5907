27998a4ae55ee73364d2260c90ef20cc5b15d3d4
SweetPainnn <lucarelly232hotmail.com>
2025-09-29 21:06:26 -0500

    Se implemento parte de la estructura del security

--- moneyProject/src/main/java/pe/edu/upc/moneyproject/repositories/IUsuarioRepository.java
+++ moneyProject/src/main/java/pe/edu/upc/moneyproject/repositories/IUsuarioRepository.java
@@ -1,8 +1,11 @@
 package pe.edu.upc.moneyproject.repositories;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.moneyproject.entities.Usuario;
 
@@ -10,6 +13,19 @@ import java.util.List;
 
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario,Integer> {
+    public Usuario findOneByNombre(String nombre);
+
+    //BUSCAR POR NOMBRE
+    @Query("select count(u.nombre) from Usuario u where u.nombre =:username")
+    public int buscarUsername(@Param("username") String nombre);
+
+
+    //INSERTAR ROLES
+    @Transactional
+    @Modifying
+    @Query(value = "insert into roles (rol, user_id) VALUES (:rol, :user_id)", nativeQuery = true)
+    public void insRol(@Param("rol") String authority, @Param("user_id") Long user_id);
+
     @Query(value = "Select u.id_usuario, u.nombre, u.correo\n" +
             "from usuario u", nativeQuery = true)
     List<String[]> findUsuarios();
