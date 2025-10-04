89123317213bd0c5ae5ee014c5c5239ce724dabe
mau <mauanto007@gmail.com>
2025-10-04 15:39:41 -0500

    final

--- moneyProject/src/main/java/pe/edu/upc/moneyproject/securities/WebSecurityConfig.java
+++ moneyProject/src/main/java/pe/edu/upc/moneyproject/securities/WebSecurityConfig.java
@@ -57,7 +57,10 @@ public class WebSecurityConfig {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req
-                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/login",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui/index.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
