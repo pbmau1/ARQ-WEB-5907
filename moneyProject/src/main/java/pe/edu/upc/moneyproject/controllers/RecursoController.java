194aa702df24a1c2ea91b1449a7e1dd53e9a75c6
mau <mauanto007@gmail.com>
2025-11-09 14:54:50 -0500

    implementacion de CORS

--- moneyProject/src/main/java/pe/edu/upc/moneyproject/controllers/RecursoController.java
+++ moneyProject/src/main/java/pe/edu/upc/moneyproject/controllers/RecursoController.java
@@ -15,6 +15,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/recurso")
 
--- moneyProject/src/main/java/pe/edu/upc/moneyproject/controllers/UsuarioController.java
+++ moneyProject/src/main/java/pe/edu/upc/moneyproject/controllers/UsuarioController.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/usuarios")
 
